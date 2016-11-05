30861956d65d2ebf78650a2736dd47079c5e5d9f
jpaulofb <jpfb@icomp.ufam.edu.br>
2016-11-05 16:38:35 -0400

    extract manga chapter urls from a html string to a file
    the extractor can get manga chapters, if a batoto html manga page is given in
    form of a string, and can save their url address and title to a file

---
+++ extractor.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import re
+from collections import namedtuple
+from BeautifulSoup import BeautifulSoup
+
+
+Link = namedtuple('Link', ['addr', 'title'])
+
+def getATag(element):
+    return element.find('a')
+
+def extractLinks(html):
+    soup = BeautifulSoup(html)
+
+    chTable = soup.find('table', {'class': re.compile(r'chapters_list')})
+    englishChs = chTable.findAll('tr', {'class': re.compile(r'lang_English')})
+    tags = map(getATag, englishChs)
+    # tags = englishChs.findAll(tag, href=re.compile(r'^https?://bato.to/reader#.*'))
+
+    links = []
+    for tag in tags:
+        # tag.contents[0] is a small book logo
+        link = Link(addr=str(tag.get('href')), title=str(tag.contents[1]).strip())
+        links.append(link)
+    return links
+
+def saveLinks(fname, links):
+    with open(fname, 'w') as fout:
+        fout.write('#addr;title\n')
+        for link in links[::-1]: # save in reverse
+            # print link
+            fout.write('{};{}\n'.format(link.addr, link.title))
+
+def extractLinksToFile(html, fname):
+    saveLinks(fname, extractLinks(html))
+
+if __name__ == '__main__':
+    html = open('examples/ps.html').read()
+    extractLinksToFile(html, 'examples/ps.csv')
