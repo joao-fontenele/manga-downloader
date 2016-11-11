b7ea9c315ba2ab24c884e19bd769a5c7eecf49e3
jpaulofb <jpfb@icomp.ufam.edu.br>
2016-11-10 20:50:59 -0400

    added cmd line functionality for extractor

--- extractor.py
+++ extractor.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 import re
+import sys
 from collections import namedtuple
 from BeautifulSoup import BeautifulSoup
 
@@ -30,12 +32,27 @@ def saveLinks(fname, links):
     with open(fname, 'w') as fout:
         fout.write('#addr;title\n')
         for link in links:
-            # print(link)
             fout.write('{};{}\n'.format(link.addr, link.title))
 
-def extractLinksToFile(html, fname):
+def extractLinksToFile(htmlFile, fname):
+    html = ''
+    with open(htmlFile) as fin:
+        html = fin.read()
     saveLinks(fname, extractLinks(html))
 
 if __name__ == '__main__':
-    html = open('examples/ps.html').read() # TODO cmd line options
-    extractLinksToFile(html, 'examples/ps.txt')
+    try:
+        path = sys.argv[1]
+    except IndexError:
+        usage = 'Usage: $ python {} <htmls folder>'.format(sys.argv[0])
+        print(usage)
+    else:
+        for fname in os.listdir(path):
+            htmlFile = os.path.join(path, fname)
+            extension = htmlFile.split('.')[-1]
+            if os.path.isfile(htmlFile) and extension == 'html':
+                outFile = htmlFile.split('.')[:-1]
+                outFile.append('txt')
+                outFile = '.'.join(outFile)
+
+                extractLinksToFile(htmlFile, outFile)
